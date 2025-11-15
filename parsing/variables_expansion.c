c05bf8a7a922f464c16354ffe80bc777331e87e5
kanye <kang2005123@gmail.com>
2025-11-15 17:21:40 +0100

    finish initial parsing initialization

--- parsing/variables_expansion.c
+++ parsing/variables_expansion.c
@@ -5,14 +5,19 @@ static char *ft_strjoin_free_s1(char *s1, char *s2)
 {
     char *new_str;
 
-    new_str = ft_strjoin(s1, s2);
+    if(s2 == NULL)
+        new_str = ft_strjoin(s1, "");
+    else
+        new_str = ft_strjoin(s1, s2);
     free(s1);
     return (new_str);
 }
 
 static char *get_var_name(const char *str, int *len)
 {
-    int i = 0;
+    int i;
+
+    i = 0;
     while (str[i] && (ft_isalnum(str[i]) || str[i] == '_'))
         i++;
     *len = i;
@@ -23,56 +28,51 @@ static char *get_var_name(const char *str, int *len)
 char *expand_variables(char *str, t_env **env, int last_exit_status)
 {
     char    *result;
-    int     i = 0;
-    int     start = 0;
-    int     in_single = 0;
-    int     in_double = 0;
+    int     i;
+    int     start;
+    int     single_quote;
+    int     double_quote ;
 
+    i = 0;
+    single_quote = 0;
+    double_quote = 0;
+    start = 0;
     result = ft_strdup("");
     while (str[i])
     {
-
-        if (str[i] == '\'' && !in_double)
-            in_single = !in_single;
-        else if (str[i] == '"' && !in_single)
-            in_double = !in_double;
-        if (str[i] == '$' && !in_single)
+        if (str[i] == '\'' && !double_quote)
+            single_quote = !single_quote;
+        else if (str[i] == '"' && !single_quote)
+            double_quote = !double_quote;
+        if (str[i] == '$' && !single_quote) //since only double quotes or no quote allows expansion
         {
-            char *chunk = ft_substr(str, start, i - start);
-            result = ft_strjoin_free_s1(result, chunk);
-            free(chunk);
-            
+            char *temporal = ft_substr(str, start, i - start);
+            result = ft_strjoin_free_s1(result, temporal);
+            free(temporal);       
             i++;
-            if (str[i] == '?')
+            if (str[i] == '?') //return last exit status
             {
                 char *status = ft_itoa(last_exit_status);
                 result = ft_strjoin_free_s1(result, status);
                 free(status);
                 i++;
             }
-            else if (ft_isalpha(str[i]) || str[i] == '_')
+            else if (ft_isalpha(str[i]) || str[i] == '_') //search for the enviroment value
             {
                 int var_len = 0;
-                char *var_name = get_var_name(&str[i], &var_len);
-                char *value = get_env_value(env, var_name); 
-                
-                result = ft_strjoin_free_s1(result, value ? value : "");
+                char *var_name = get_var_name(&str[i], &var_len); //like HOME
+                char *value = get_env_value(env, var_name); //not free this variable since we did not add any malloc in this function
+                result = ft_strjoin_free_s1(result, value);
                 free(var_name);
                 i += var_len;
             }
-            else
-            {
+            else //if the enviroment is not a valid one
                 result = ft_strjoin_free_s1(result, "$");
-            }
-            
             start = i;
         }
         else
-        {
             i++;
-        }
     }
- 
     char *last_chunk = ft_substr(str, start, i - start);
     result = ft_strjoin_free_s1(result, last_chunk);
     free(last_chunk);
@@ -90,9 +90,7 @@ void replace_tokens_variables(t_token *token_list, t_env **env, int last_exit_st
     while (current)
     {
         if (current->type == TOKEN_WORD)
-        {
-            current->str = expand_variables(current->str, env, last_exit_status);;
-        }
+            current->str = expand_variables(current->str, env, last_exit_status);
         current = current->next;
     }
 }
