5a859e5561a3af4b16f491678c68896a7558287d
Israel <israelguerreroortiz83@gmail.com>
2025-11-16 23:52:58 +0100

    feat: handle pipes and unify execution logic

--- execute/execute.c
+++ execute/execute.c
@@ -6,41 +6,62 @@
 /*   By: iisraa11 <iisraa11@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/30 10:58:49 by iisraa11          #+#    #+#             */
-/*   Updated: 2025/11/04 11:37:49 by iisraa11         ###   ########.fr       */
+/*   Updated: 2025/11/16 19:42:28 by iisraa11         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-static void exec_single(t_cmd *cmd, char **envp)
+static void child_proccess(t_cmd *cmd, char **envp, int prev_fd, int pipe_fd[2])
 {
-    pid_t pid = fork();
-
-    if (pid == 0)
+    char *path;
+    if (prev_fd != -1)
     {
-        char *path = find_cmd_path(cmd->argv[0], envp);
-        if (!path)
-        {
-            printf("minishell: command not found: %s\n", cmd->argv[0]);
-            exit(1);
-        }
-        if (cmd->infile || cmd->outfile)
-            handle_redirections(cmd);
-        execve(path, cmd->argv, envp);
-        perror("execve");
+        dup2(prev_fd, STDIN_FILENO);
+        close(prev_fd);
+    }
+    if (cmd->next)
+    {
+        close(pipe_fd[0]);
+        dup2(pipe_fd[1], STDOUT_FILENO);
+        close(pipe_fd[1]);
+    }
+    path = find_cmd_path(cmd->argv[0], envp);
+    if (!path)
+    {
+        printf("minishell: command not found: %s\n", cmd->argv[0]);
         exit(1);
     }
-    else
-        waitpid(pid, NULL, 0);
+    if (cmd->infile || cmd->outfile)
+        handle_redirections(cmd);
+    execve(path, cmd->argv, envp);
+    exit(1);
 }
 
 void executor(t_cmd *cmd_list, char **envp)
 {
-    if (!cmd_list)
-        return;
-    if (cmd_list->next == NULL)
+    t_cmd *cmd;
+    int prev_fd;
+    int pipe_fd[2];
+    pid_t pid;
+
+    cmd = cmd_list;
+    prev_fd = -1;
+    while (cmd)
     {
-        exec_single(cmd_list, envp);
-        return;
+        if (cmd->next)
+            pipe(pipe_fd);
+        pid = fork();
+        if (pid == 0)
+            child_proccess(cmd, envp, prev_fd, pipe_fd);
+        if (prev_fd != -1)
+            close(prev_fd);
+        if (cmd->next)
+        {
+            close(pipe_fd[1]);
+            prev_fd = pipe_fd[0];
+        }
+        cmd = cmd->next;
     }
+    while (wait(NULL) > 0);
 }
